1f2021a847a23c898bb17f68647d0b5d09b664f8
darckfast <21337578+Darckfast@users.noreply.github.com.>
2025-02-19 16:09:36 -0300

    feat: add gzip encoding

--- pkg/utils/reverse.go
+++ pkg/utils/reverse.go
@@ -1,11 +1,13 @@
 package utils
 
 import (
+	"compress/gzip"
 	"context"
 	"io"
 	"log/slog"
 	"net/http"
 	"os"
+	"strings"
 
 	multilogger "github.com/Darckfast/multi_logger/pkg/multi_logger"
 	"github.com/syumai/workers/cloudflare/fetch"
@@ -31,9 +33,17 @@ func DoReverseProxy(ctx context.Context, remoteUrl string, w http.ResponseWriter
 
 	defer resp.Body.Close()
 
-	io.Copy(w, resp.Body)
-	w.WriteHeader(resp.StatusCode)
+	if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+		w.Header().Set("Content-Encoding", "gzip")
+		gz := gzip.NewWriter(w)
+		defer gz.Close()
 
+		io.Copy(gz, resp.Body)
+	} else {
+		io.Copy(w, resp.Body)
+	}
+
+	w.WriteHeader(resp.StatusCode)
 	w.Header().Add("Content-Type", r.Header.Get("Content-Type"))
 	w.Header().Add("Content-Length", r.Header.Get("Content-Length"))
 	w.Header().Add("Cache-Control", r.Header.Get("Cache-Control"))
